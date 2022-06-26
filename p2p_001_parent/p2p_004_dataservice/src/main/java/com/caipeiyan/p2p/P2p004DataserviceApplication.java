4622b01bfac6b703b91653ce97c9186afa67746c
show <1049661367@qq.com>
2022-06-27 02:25:29 +0800

    version 1.0.1

---
+++ p2p_001_parent/p2p_004_dataservice/src/main/java/com/caipeiyan/p2p/P2p004DataserviceApplication.java
@@ -0,0 +1,13 @@
+package com.caipeiyan.p2p;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2p004DataserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(P2p004DataserviceApplication.class, args);
+    }
+
+}
